df33275fbbf8dfbb447b44747b7212451927fe68
AbyssSkb <abyssskb@outlook.com>
2025-08-05 21:04:28 +0800

    feat(lab02): complete exercise 2

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -1,10 +1,12 @@
+// clang-format off
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lfsr.h"
+// clang-format on
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+void lfsr_calculate(uint16_t* reg)
+{
+    *reg = (*reg >> 1) | ((*reg & 1) ^ ((*reg >> 2) & 1) ^ ((*reg >> 3) & 1) ^ ((*reg >> 5) & 1)) << 15;
 }
-
